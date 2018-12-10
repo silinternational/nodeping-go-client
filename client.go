9f4df5ef64373d3aa2984373762cd82fe89af09b
Baggerone <baggerone@gmail.com>
2018-12-10 15:13:14 -0500

    Revert "Feature/mock tests"

--- client.go
+++ client.go
@@ -3,7 +3,6 @@ package nodeping
 import (
 	"fmt"
 	"gopkg.in/resty.v1"
-	"encoding/json"
 )
 
 const BaseURL = "https://api.nodeping.com/api/1"
@@ -20,10 +19,9 @@ var Client NodePingClient
 
 // NodePingClient holds config and provides methods for various api calls
 type NodePingClient struct {
-	Config      ClientConfig
-	Error       NodePingError
-	R           *resty.Request
-	MockResults string
+	Config ClientConfig
+	Error  NodePingError
+	R      *resty.Request
 }
 
 // Initialize new NodePingClient
@@ -39,7 +37,6 @@ func New(config ClientConfig) (*NodePingClient, error) {
 	}
 
 	Client.Config.CustomerID = config.CustomerID
-	Client.MockResults = ""
 
 	resty.SetHostURL(Client.Config.BaseURL)
 	resty.SetBasicAuth(Client.Config.Token, "")
@@ -50,23 +47,16 @@ func New(config ClientConfig) (*NodePingClient, error) {
 	return &Client, nil
 }
 
-// ListChecks retrieves all the "Checks" in NodePing
 func (c *NodePingClient) ListChecks() ([]CheckResponse, error) {
 	path := "/checks"
 	if c.Config.CustomerID != "" {
 		path = fmt.Sprintf("/checks/%s", c.Config.CustomerID)
 	}
 	var listObj map[string]CheckResponse
-
-
-	if c.MockResults != "" {
-		json.Unmarshal([]byte(c.MockResults), &listObj)
-	} else {
-		_, err := c.R.SetResult(&listObj).Get(path)
-		errChk := CheckForError(err, c)
-		if errChk != nil {
-			return []CheckResponse{}, errChk
-		}
+	_, err := c.R.SetResult(&listObj).Get(path)
+	errChk := CheckForError(err, c)
+	if errChk != nil {
+		return []CheckResponse{}, errChk
 	}
 
 	var list []CheckResponse
@@ -77,16 +67,9 @@ func (c *NodePingClient) ListChecks() ([]CheckResponse, error) {
 	return list, nil
 }
 
-// GetCheck retrieves data about one Check using its id
 func (c *NodePingClient) GetCheck(id string) (CheckResponse, error) {
 	path := fmt.Sprintf("/checks/%s", id)
 	var check CheckResponse
-
-	if c.MockResults != "" {
-		json.Unmarshal([]byte(c.MockResults), &check)
-		return check, nil
-	}
-
 	_, err := c.R.SetResult(&check).Get(path)
 	errChk := CheckForError(err, c)
 	if errChk != nil {
@@ -94,65 +77,6 @@ func (c *NodePingClient) GetCheck(id string) (CheckResponse, error) {
 	}
 
 	return check, nil
-}
-
-// GetUptime retrieves the uptime entries for a certain check within an optional date range (by Timestamp with microseconds)
-func (c *NodePingClient) GetUptime(id string, start, end int64) (map[string]UptimeResponse, error) {
-	// Build path with potentially a "?" and "&" symbols
-	// I'm not getting c.R's built in query param functions to work properly
-	pathDelimiter := ""
-	queryParams := ""
-	queryParamDelimiter := ""
-
-	if start > 0 {
-		queryParams = fmt.Sprintf("start=%d", start)
-		queryParamDelimiter = "&"
-		pathDelimiter = "?"
-	}
-
-	if end > 0 {
-		queryParams = fmt.Sprintf("%s%send=%d", queryParams, queryParamDelimiter, end)
-		pathDelimiter = "?"
-	}
-
-	path := fmt.Sprintf("/results/uptime/%s%s%s", id, pathDelimiter, queryParams)
-
-	var listObj map[string]UptimeResponse
-
-	if c.MockResults != "" {
-		json.Unmarshal([]byte(c.MockResults), &listObj)
-		return listObj, nil
-	}
-
-	_, err := c.R.SetResult(&listObj).Get(path)
-	errChk := CheckForError(err, c)
-	if errChk != nil {
-		return map[string]UptimeResponse{}, errChk
-	}
-
-	return listObj, nil
-}
-
-// ListContactGroups retrieves the list of Contact Groups
-func (c *NodePingClient) ListContactGroups() (map[string]ContactGroupResponse, error) {
-	path := "/contactgroups"
-	if c.Config.CustomerID != "" {
-		path = fmt.Sprintf("/checks/%s", c.Config.CustomerID)
-	}
-	var listObj map[string]ContactGroupResponse
-
-	if c.MockResults != "" {
-		json.Unmarshal([]byte(c.MockResults), &listObj)
-		return listObj, nil
-	}
-
-	_, err := c.R.SetResult(&listObj).Get(path)
-	errChk := CheckForError(err, c)
-	if errChk != nil {
-		return map[string]ContactGroupResponse{}, errChk
-	}
-
-	return listObj, nil
 }
 
 func CheckForError(err error, client *NodePingClient) error {
